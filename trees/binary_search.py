274f2d6e6d4cea7172f1dcc281c01d71b3879885
Rodrigo <rodrigorom2000@gmail.com>
2023-12-14 17:48:51 -0500

    binary search solution

---
+++ trees/binary_search.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/binary-search/
+
+def search(nums: List[int], target: int) -> int:
+    l, r = 0, len(nums)-1
+    while l <= r:
+        m = (r + l) // 2
+        if nums[m] > target:
+            r = m + 1
+        elif nums[m] < target:
+            l = m + 1
+        else:
+            return m 
+    return -1
