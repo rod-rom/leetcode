1ee51d6234e84f748bda68a309fac88903ef5e05
rromer07 <rodrigorom2000@gmail.com>
2023-12-28 02:15:03 -0500

    maximum depth of binary tree cpp solution

--- trees/cpp/maximum_depth_of_binary_tree.cpp
+++ trees/cpp/maximum_depth_of_binary_tree.cpp
@@ -14,6 +14,7 @@ struct TreeNode {
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-    
+	if (!root) { return 0; }
+	return 1 + max(maxDepth(root->left), maxDepth(root->right));
     }
 };
